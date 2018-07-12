9f32251f4a560756b1298aeb6fcc5d9181ba5255
LBB <liubbpro@gmail.com>
2018-07-12 21:33:03 +0800

    config.txt

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -1,3 +1,33 @@
+#include "utils.h"
+#include "peripherals/mini_uart.h"
+#include "peripherals/gpio.h"
+
+
+void uart_send(char c)
+{
+    while(1){
+        if(get32(AUX_MU_LSR_REG)&0x20)
+            break;
+    }
+    put32(AUX_MU_IO_REG, c);
+}
+
+char uart_recv(void)
+{
+    while(1){
+        if(get32(AUX_MU_LSR_REG)&0x01)
+            break;
+    }
+    return(get32(AUX_MU_IO_REG)&0xff);
+}
+
+void uart_send_string(char* str)
+{
+    for(int i = 0; str[i] != '\0'; i++){
+        uart_send((char)str[i]);
+    }
+}
+
 void uart_init(void)
 {
     unsigned int selector;
@@ -19,5 +49,9 @@ void uart_init(void)
     put32(AUX_MU_CNTL_REG, 0);              //Disable auto flow control and disable receiver and transmitter (for now)
     put32(AUX_MU_IER_REG, 0);               //Disable receive and transmit interrupts
     put32(AUX_MU_LCR_REG, 3);               //Enable 8 bit mode
-    
+    put32(AUX_MU_MER_REG, 0);               //Set RTS line to be always high
+    put32(AUX_MU_BAUD_REG, 270);            //Set baud rate to 115200
+    put32(AUX_MU_IIR_REG, 6);               //Clear FIFO
+
+    put32(AUX_MU_CNTL_REG, 3);              //Finally, enable transmitter and receiver
 }
