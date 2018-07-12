f08ec36ebc76b1c5e4e47ef79e272743c2dc3c91
LBB <liubbpro@gmail.com>
2018-07-12 15:46:51 +0800

    init uart

--- src/mini_uart.c
+++ src/mini_uart.c
@@ -8,4 +8,16 @@ void uart_init(void)
     selector &= ~(7<<15);                   //clean gpio15
     selector |= 2<<15;                      //set alt5 for gpio15
     put32(GPFSEL1, selector);
+
+    put32(GPPUD, 0);
+    delay(150);
+    put32(GPPUDCLK0, (1<<14)|(1<<15));
+    delay(150);
+    put32(GPPUDCLK0, 0);
+
+    put32(AUX_ENABLES, 1);                  //Enable mini uart (this also enables access to it register)
+    put32(AUX_MU_CNTL_REG, 0);              //Disable auto flow control and disable receiver and transmitter (for now)
+    put32(AUX_MU_IER_REG, 0);               //Disable receive and transmit interrupts
+    put32(AUX_MU_LCR_REG, 3);               //Enable 8 bit mode
+    
 }
